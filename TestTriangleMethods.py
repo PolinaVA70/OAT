7aa401a6aa6afe1f9cf6e6bb968095a0d052b0c1
PolinaAfonina <teks_samara@mail.ru>
2023-04-09 18:17:01 +0400

    класс Ttiangle

---
+++ TestTriangleMethods.py
@@ -0,0 +1,26 @@
+import unittest
+import Triangle
+
+class TestTriangleMethods(unittest.TestCase):
+
+    def setUp(self):
+        #print("setUp")
+        self.a = "3"
+        self.b = "3"
+        self.c = "3"
+        self.test_triangle = Triangle.Triangle(self.a, self.b, self.c, 60, 60, 60)
+
+    def test_init(self):
+        print("test_init")
+        self.assertEqual(self.test_triangle.a, float(self.a))
+        self.assertEqual(self.test_triangle.b, float(self.b))
+        self.assertEqual(self.test_triangle.c, float(self.c))
+
+    def test_S(self):
+        p = (float(self.a) + float(self.b) + float(self.c)) /2
+        S = (p*(p-float(self.a))*(p-float(self.b))*(p-float(self.c)))**0.5
+        test_triangle = Triangle.Triangle(float(self.a), float(self.b), float(self.c), 60, 60, 60)
+        test_triangle.s() # считаем площадь
+        print(int(test_triangle.S))
+        self.assertEqual(int(test_triangle.S), int(S))
+    # def test_s_negative(self)
---
+++ Triangle.py
@@ -0,0 +1,24 @@
+class Triangle:
+
+    def __init__(self, a, b, c, A, B, C): #инициализатор - момент создания объекта
+        self.a = float (a)  # self - свойства, параметры, атрибуты, характеристика
+        self.b = float (b)
+        self.c = float (c)
+        self.A = A
+        self.B = B
+        self.C = C
+        self.S = 0
+
+    def s(self):    # фукнцмия внутри класса, это и есть метод
+        a = self.a
+        b = self.b
+        c = self.c
+        p = (a + b + c)/2
+        #return (p*(p-a)*(p-b)*(p-c))**0.5 # вернет подсчет площади
+        self.S = (p*(p-a)*(p-b)*(p-c))**0.5
+#a = "3"
+#print(float(a))
+triangle = Triangle(3, 3, 3, 60, 60, 60)
+triangle.s()
+print(triangle.a, triangle.b, triangle.c, triangle.S)
+
